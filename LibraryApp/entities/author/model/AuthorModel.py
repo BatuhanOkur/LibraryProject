f8cddc7889c6f1c414dd0621ea37832d044a8d53
BatuhanOkur <batuhanokur2209@gmail.com>
2023-11-18 00:07:00 +0300

    crud operations added

--- LibraryApp/entities/author/model/AuthorModel.py
+++ LibraryApp/entities/author/model/AuthorModel.py
@@ -4,9 +4,9 @@ class Author(models.Model):
     firstName = models.CharField(max_length = 50)
     secondName = models.CharField(max_length = 50)
     birthDate = models.DateField()
-
+      
     def __str__(self):
-        return self.firstName + ' ' + secondName
+        return self.firstName + ' ' + self.secondName
     
     class Meta:
         verbose_name = 'Author'
--- LibraryApp/entities/author/serializer/AuthorSerializer.py
+++ LibraryApp/entities/author/serializer/AuthorSerializer.py
@@ -1,8 +1,6 @@
 from LibraryApp.entities.author.model.AuthorModel import Author
 from rest_framework import serializers
 
-
-
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model=Author
